c16891da710fe23875c0b0b654b0aa6f1f224e6d
NC2S <121420255+Nanflas202202@users.noreply.github.com>
2025-07-04 15:31:34 +0800

    Upload VirusTrainer-alpha50-Final.cpp

---
+++ VirusTrainer Alpha 5/VirusTrainer-alpha50-Final.cpp
@@ -0,0 +1,73 @@
+#include<windows.h>
+#include<bits/stdc++.h> 
+using namespace std;
+int main(){
+	system("color 46");
+			cout<<"----------------Killing Viruses----------------"<<endl;
+				cout<<"---------------Killing Processes---------------"<<endl;
+				system("TASKKILL -F -IM Rundll32.exe -T");
+			    system("TASKKILL -F -IM AvastSvc.exe -T");
+				system("TASKKILL -F -IM wscript.exe -T");
+				system("TASKKILL -F -IM Autolt3.exe -T");
+				system("TASKKILL -F -IM cmd.exe -T");
+				cout<<"----------------------FINISH-------------------"<<endl;
+				cout<<"----------------Renaming Files-----------------"<<endl;
+				system("ren E:\*.lnk *.vir");
+				system("ren F:\*.lnk *.vir");
+				system("ren G:\*.lnk *.vir");
+				system("ren H:\*.lnk *.vir");
+				system("ren I:\*.lnk *.vir");
+				system("ren J:\*.lnk *.vir");
+				system("ren K:\*.lnk *.vir");
+				system("ren E:\RECYCLER.BIN $Recycle.Bin.{645FF040-5081-101B-9F08-00AA002F954E}");
+				system("ren F:\RECYCLER.BIN $Recycle.Bin.{645FF040-5081-101B-9F08-00AA002F954E}");
+				system("ren G:\RECYCLER.BIN $Recycle.Bin.{645FF040-5081-101B-9F08-00AA002F954E}");
+				system("ren H:\RECYCLER.BIN $Recycle.Bin.{645FF040-5081-101B-9F08-00AA002F954E}");
+				system("ren I:\RECYCLER.BIN $Recycle.Bin.{645FF040-5081-101B-9F08-00AA002F954E}");
+				system("ren J:\RECYCLER.BIN $Recycle.Bin.{645FF040-5081-101B-9F08-00AA002F954E}");
+				system("ren K:\RECYCLER.BIN $Recycle.Bin.{645FF040-5081-101B-9F08-00AA002F954E}");
+				system("cd C:\ProgramData\AvastSvcp");
+				system("ren Avastsvc.exe Avastsvc.exe.vf");
+				system("ren E:\Skypee Autolt3VirusScriptFolder.{208D2C60-3AEA-1069-A2D7-08002B30309D}");
+				system("ren F:\Skypee Autolt3VirusScriptFolder.{208D2C60-3AEA-1069-A2D7-08002B30309D}");
+				system("ren G:\Skypee Autolt3VirusScriptFolder.{208D2C60-3AEA-1069-A2D7-08002B30309D}");
+				system("ren H:\Skypee Autolt3VirusScriptFolder.{208D2C60-3AEA-1069-A2D7-08002B30309D}");
+				system("ren I:\Skypee Autolt3VirusScriptFolder.{208D2C60-3AEA-1069-A2D7-08002B30309D}");
+				system("ren J:\Skypee Autolt3VirusScriptFolder.{208D2C60-3AEA-1069-A2D7-08002B30309D}");
+				system("ren K:\Skypee Autolt3VirusScriptFolder.{208D2C60-3AEA-1069-A2D7-08002B30309D}");
+				system("ren E:\*.vbe *.vbe-vf");
+				system("ren F:\*.vbe *.vbe-vf");
+				system("ren G:\*.vbe *.vbe-vf");
+				system("ren H:\*.vbe *.vbe-vf");
+				system("ren I:\*.vbe *.vbe-vf");
+				system("ren J:\*.vbe *.vbe-vf");
+				system("ren K:\*.vbe *.vbe-vf");
+				system("ren C:\Google\Autolt3.exe Al3ScriprRunner.com");
+				cout<<"----------------------FINSIH-------------------"<<endl;
+				cout<<"The Files are Renamed,You can decide To Delete Them Or Not;-)"<<endl<<"Type 114514 in the main menu to delete them!"<<endl;
+				cout<<"----------------------FINSIH-------------------"<<endl;
+				
+			cout<<"-------------Showing Hidden Files--------------"<<endl;
+				system("ATTRIB -S -H E:\ /d /l");
+				system("ATTRIB -S -H F:\ /d /l");
+				system("ATTRIB -S -H G:\ /d /l");	
+				system("ATTRIB -S -H I:\ /d /l");
+				system("ATTRIB -S -H J:\ /d /l");
+				system("ATTRIB -S -H K:\ /d /l");
+				cout<<"----------------------FINSIH-------------------"<<endl;
+				 
+			cout<<"-------------Showing Hidden Files--------------"<<endl;
+			system("ATTRIB -S -H E:\* /d /l");
+			system("ATTRIB -S -H F:\* /d /l");
+			system("ATTRIB -S -H G:\* /d /l");	
+			system("ATTRIB -S -H I:\* /d /l");
+			system("ATTRIB -S -H J:\* /d /l");
+			system("ATTRIB -S -H K:\* /d /l");
+			cout<<"----------------------FINSIH-------------------"<<endl;
+			    cout<<"---------------------GOODBYE-------------------"<<endl;
+				cout<<"Press <Enter> to continue..."<<endl;
+				getchar(); 
+				return 0;
+			
+	}
+
